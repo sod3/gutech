b4e01f2ceaf72fe9f7df2091be4ad41a5b25d473
sod3 <mudassir.muneer123@gmail.com>
2024-11-26 11:08:39 +0500

    task6

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Content.css';
+
+const Content = () => {
+  return (
+    <div className="content">
+      <p>Mudassir and Web Tech LAB</p>
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Greeting.css'
+const Greeting = () => {
+  return (
+    <div className="greeting">
+      <h1>Hello, Mudassir! Welcome to React!</h1>
+    </div>
+  );
+};
+
+export default Greeting;
+
