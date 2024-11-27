13e25ee0ca93286f6bf789b4e5e84c035448595c
sod3 <mudassir.muneer123@gmail.com>
2024-11-27 17:14:46 +0500

    lab7

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import Greeting from './components/Greeting';
+// import Greeting from './components/Greeting';
 import './App.css';
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
-
+// import Header from './components/Header';
+// import Content from './components/Content';
+// import Footer from './components/Footer';
+import Navbar from './components/lab7/Navbar';
+import LandingPage from './components/lab7/LandingPage';
+import Form from './components/lab7/Form';
+import Footer from './components/lab7/Footer';
 const App = () => {
   return (
     <div className="App">
-      <Header />
+      {/* <Header />
       <Greeting />
       <Content />
+      <Footer /> */}
+      <Navbar />
+      <LandingPage />
+      <Form />
       <Footer />
     </div>
   );
---
+++ src/components/lab7/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2024 YouConect. All rights reserved.</p>
+        <p>
+          Created by{' '}
+          <a
+            href="https://www.linkedin.com/in/mudassir-muneer-webdeveloperexpert/"
+          >
+            Mudassir Muneer
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
